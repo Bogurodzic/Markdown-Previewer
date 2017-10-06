fdd52d391d5d79debcedc996532956ed1eeb630a
Kamil Jarzab <jarzbkamil@gmail.com>
2017-10-06 11:09:16 +0200

    Added components and bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MarkdownInputComponent } from './markdown-input/markdown-input.component';
+import { MarkdownOutputComponent } from './markdown-output/markdown-output.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MarkdownInputComponent,
+    MarkdownOutputComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/markdown-output/markdown-output.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-markdown-output',
+  templateUrl: './markdown-output.component.html',
+  styleUrls: ['./markdown-output.component.css']
+})
+export class MarkdownOutputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
